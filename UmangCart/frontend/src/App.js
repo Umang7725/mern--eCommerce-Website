1b4c369c4d6b984e94d9c0ff5e586237e3923539
umang <umangraj7725@gmail.com>
2023-04-11 19:25:21 +0530

    day-5-Create-Git-Repository

--- UmangCart/frontend/src/App.js
+++ UmangCart/frontend/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -8,7 +10,23 @@ function App() {
         <a href="/">UmangCart</a>
       </header>
       <main>
-        List Product
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/ ${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+              <a href={`/product/ ${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
