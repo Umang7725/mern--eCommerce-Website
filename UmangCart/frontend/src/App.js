9617b622b2c90e3954e2d53e978f3e197d4802d7
umang <umangraj7725@gmail.com>
2023-04-11 18:19:44 +0530

    Day-03-Create-React-App

---
+++ UmangCart/frontend/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">UmangCart</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
