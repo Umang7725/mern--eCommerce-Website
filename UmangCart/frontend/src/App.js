954604cd06adf3e5362555501623f5534d8ca9b6
umang <umangraj7725@gmail.com>
2023-04-14 18:45:24 +0530

    Day-10 Added Bootstrap in the project

--- UmangCart/frontend/src/App.js
+++ UmangCart/frontend/src/App.js
@@ -5,21 +5,36 @@ import {BrowserRouter,Link,Route, Routes} from 'react-router-dom';
 // import data from "./data";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/COntainer';
+import {LinkContainer} from 'react-router-bootstrap';
+
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to="/">UmangCart</Link>
+        <Navbar bg='dark' variant='dark'>
+          <Container >
+            <LinkContainer to="/">
+            <Navbar.Brand>UmangCart</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+        {/* <Link to="/">UmangCart</Link> */}
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path='/product/:slug' element ={<ProductScreen/>} />
           <Route path='/' element={<HomeScreen/>} />
         </Routes>
-        
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>Created by Umang Raj LPU 12018505</div>
+      </footer>
     </div>
   </BrowserRouter>
     );
