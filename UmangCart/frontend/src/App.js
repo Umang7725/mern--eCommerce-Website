f9518644fd3d1a1c885cfa0076c4b387170ad551
umang <umangraj7725@gmail.com>
2023-04-24 22:55:05 +0530

    Day-32 Create Profile Screen

--- UmangCart/frontend/src/App.js
+++ UmangCart/frontend/src/App.js
@@ -23,6 +23,8 @@ import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
 
 
 function App() {
@@ -36,6 +38,7 @@ function App() {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
   };
 
   return (
@@ -95,6 +98,7 @@ function App() {
           <Route path="/cart" element={<CartScreen />} />
           <Route path="/signin" element={<SigninScreen />} />
           <Route path="/signup" element={<SignupScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
           <Route path="/placeorder" element={<PlaceOrderScreen />} />
           <Route path="/order/:id" element={<OrderScreen />}/>
           <Route path="/orderhistory" element={<OrderHistoryScreen />}/>
